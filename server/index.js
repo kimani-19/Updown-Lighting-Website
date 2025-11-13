adf6ee6493da50d1106eb3e66edf2e04f3caaf0c
kimaniMunn <kmunn2024@fau.edu>
2025-11-12 20:40:53 -0500

    Fix: Serve production build of client

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Google Reviews Route
 app.get('/api/reviews', async (req, res) => {
@@ -263,7 +263,7 @@ app.post('/api/chatbot', (req, res) => {
 
 // Serve React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Start server
